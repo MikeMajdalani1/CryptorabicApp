af8f45e7eb0b2fc3c00c37dfaa1be9b3d36dfd20
Mike-Laptop <agent47mike47@gmail.com>
2022-10-01 13:17:59 +0300

    default template

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cryptorabic.com',
+  appName: 'cryptorabic',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
